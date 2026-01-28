de56bf62f67f2d48caa8c7a2b0ee62a3d0fb1db8
Diego Lara <me@delg.dev>
2026-01-28 10:34:54 -0600

    Add new route to serve 2nd.html for extra content

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/extra', (req, res) => {
+    res.sendFile(__dirname + '/2nd.html');
+});
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
